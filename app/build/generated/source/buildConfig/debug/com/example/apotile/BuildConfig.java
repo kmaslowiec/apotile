0c6fb7accd7b721bdcbf40676dcd3a170a28fca5
Konrad <kmaslowiec@gmail.com>
2020-11-26 21:55:40 +0100

    First Commit. Gitignore setup.

---
+++ app/build/generated/source/buildConfig/debug/com/example/apotile/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.apotile;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.apotile";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
